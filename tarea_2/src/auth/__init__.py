4352d6e9b916dafded63a097fda82987f3ef1544
admeneses <alejandrocardie12@gmail.com>
2023-02-09 22:42:49 -0400

    add create_all()

--- tarea_2/src/auth/__init__.py
+++ tarea_2/src/auth/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from config import Config
+#from flask_migrate import Migrate
 from auth.extensions import db
 
 def create_app(config_class=Config):
@@ -7,6 +8,10 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     db.init_app(app)
+    #migrate.init_app(app, db)
+    from .models.user import User
+    with app.app_context():
+        db.create_all()
 
     from auth.main import bp as main_bp
     app.register_blueprint(main_bp)
