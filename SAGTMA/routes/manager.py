2225a322e521b2da878d4406a5ac32d46c30b59c
admeneses <alejandrocardie12@gmail.com>
2023-03-01 22:32:10 -0400

    progress in the implementation of the project module added

--- SAGTMA/routes/manager.py
+++ SAGTMA/routes/manager.py
@@ -1,14 +1,81 @@
-from flask import Response, abort, current_app, render_template
+from flask import (Response, current_app, flash, redirect, render_template,
+                   request, url_for, json)
 
-from SAGTMA.utils.decorators import requires_roles
+from SAGTMA.utils import project, events
+from SAGTMA.utils.decorators import login_required, requires_roles
 
+from SAGTMA.models import Project, db
 
 @current_app.route('/project-portfolio/',methods=['GET', 'POST'])
 @requires_roles('Gerente de Operaciones')
 def portfolio() -> Response:
-    return render_template('manager/portfolio.html')
+    '''Muestra la lista de proyectos anadidos en el sistema'''
+        
+    # SE ASUME QUE ES METDO GET, FALTA BARRA DE BUSQUEDA
+    # Selecciona los proyectos de la base de datos
+    stmt = db.select(Project)
+
+    result = db.session.execute(stmt).fetchall()
+    projects = [r for r, in result]
+
+    return render_template('manager/portfolio.html', projects=projects)
 
 @current_app.route('/create-project/',methods=['GET', 'POST'])
+@login_required
 @requires_roles('Gerente de Operaciones')
 def create_project() -> Response:
-    abort(404)
+    '''Crear y anade un proyecto en la base de datos.'''
+    if request.method == "POST":
+        description = request.form['description']
+        start_date = request.form['start_date']
+        deadline = request.form['deadline']
+        
+        try:
+            project.create_project(description, start_date, deadline)
+        except project.CreateProjectError as e:
+            flash(f'{e}')
+        
+    # Se permanece en la página
+    return redirect(url_for('portfolio'))
+
+@current_app.route('/modify-project/<int:project_id>', methods=['POST'])
+@login_required
+@requires_roles('Gerente de Operaciones')
+def modify_project(project_id):
+    '''Modifica los datos de un proyecto en la base de datos'''
+    description = request.form['description']
+    start_date = request.form['start_date']
+    deadline = request.form['deadline']
+
+    try:
+        project.modify_project(project_id, description, start_date, deadline)
+    except project.CreateProjectError as e:
+        flash(f'{e}')
+
+    # Se permanece en la pagina
+    return redirect(url_for('portfolio'))
+
+@current_app.route('/select', methods=['GET', 'POST'])
+@login_required
+@requires_roles('Gerente de Operaciones')
+def select():
+    if request.method == 'POST':
+        project_id = request.form['project_id']
+        stmt = db.select(Project).where(Project.id == project_id) #FALTA VERIFICAR
+        rproject = db.session.execute(stmt).first()[0]
+
+        project_list = [{
+            'id': rproject.id,
+            'description': rproject.description,
+            'start_date': rproject.start_date.strftime("%Y-%m-%d"),
+            'deadline': rproject.end_date.strftime("%Y-%m-%d")
+        }]
+
+        return json.dumps(project_list)
+
+from urllib.parse import unquote as urllib_unquote
+
+@current_app.template_filter('unquote')
+def unquote(url):
+    safe = app.jinja_env.filters['safe']
+    return safe(urllib_unquote(url))
---
+++ SAGTMA/utils/project.py
@@ -0,0 +1,133 @@
+from typing import Tuple
+from datetime import date
+
+import bcrypt
+
+from SAGTMA.models import Project, User, db
+from SAGTMA.utils import events
+
+
+# ========== Excepciones ==========
+class CreateProjectError(ValueError):
+    pass
+
+class AlreadyExistingProjectError(CreateProjectError):
+    pass
+
+class InvalidDescripProjectError(CreateProjectError):
+    pass
+
+class InvalidProjectDate(CreateProjectError):
+    pass
+
+class MissingFieldError(CreateProjectError):
+    pass
+
+# ========== Validaciones ==========
+def validate_descrip_project(description: str) -> bool:
+    '''Lanza una excepción si la descripcion de un proyecto no es valida.
+
+    Una descripcion de un proyecto es válida si:
+      -Tiene al menos 10 caracteres y a lo sumo 200 caracteres
+      -No tiene caracteres especiales distintos de '_' (guión bajo)
+      -No comienza con un caracter numérico
+    '''
+    if len(description) < 10 or len(description) > 200:
+            raise InvalidDescripProjectError('La descripcion del proyecto debe tener entre 10 y 200 caracteres.')
+
+    if description[0].isdigit():
+        raise InvalidDescripProjectError('La descripcion de un proyecto no puede comenzar con un número.')
+
+    for char in description:
+        if not char.isalnum() and char != '_' and char != ' ':
+            raise InvalidDescripProjectError('La descripcion de un proyeto no puede contener caracteres especiales.')
+
+# Falta tipo de parametro
+def validate_date(start_date, deadline) -> bool:
+    '''Lanza una excepción si la fecha de inicio de un proyecto es despues que su fecha de cierre'''
+
+    if start_date > deadline:
+        raise InvalidProjectDate('La fecha de inicio no puede ser mayor que la fecha de cierre.')
+
+# ========== Anadidura ==========
+# Falta tipo de parametro
+def create_project(
+    description: str,
+    start_date,
+    deadline
+):
+    '''Crea y anade un usuario en la base de datos.
+
+    Lanza una excepción CreateProyectError si hubo algún error.
+    '''
+    if not all([description, start_date, deadline]):
+        raise MissingFieldError('Todos los campos son obliglatorios')
+
+    # Verifica si ya existe un proyecto con la mima descripcion
+    stmt = db.select(Project).where(Project.description == description)
+    if db.session.execute(stmt).first():
+        raise AlreadyExistingProjectError('El proyecto ya existe')
+    
+    # Eliminar espacios al comienzo y final de la descripcion
+    description = description.strip()
+
+    # Convert fechas a tipo Date
+    y, m, d = start_date.split('-')
+    start_date_t = date(int(y), int(m), int(d))
+
+    y, m, d = deadline.split('-')
+    deadline_t = date(int(y), int(m), int(d))
+
+    # Chequea si los campos ingresados son válidos
+    validate_descrip_project(description)
+    validate_date(start_date_t, deadline_t)
+
+    # Crea el proyecto en la base de datos
+    new_project = Project(description, start_date_t, deadline_t)
+    db.session.add(new_project)
+
+    # Registra el evento en la base de datos
+    events.add_project(new_project.description)
+
+# ========== Modificar ==========
+def modify_project(
+    project_id: int, 
+    description: str, 
+    start_date, 
+    deadline
+):
+    '''Modifica los datos de un proyecto en la base de datos
+        
+    Lanza una excepción CreateProyectError si hubo algún error.
+    '''
+    if not all([description, start_date, deadline]):
+        raise MissingFieldError('Todos los campos son obliglatorios')
+
+    # Verifica si ya existe un proyecto con la mima descripcion
+    stmt = db.select(Project).where(Project.description == description)
+    if db.session.execute(stmt).first():
+        raise AlreadyExistingProjectError('El proyecto ya existe')
+
+    # Busca el proyecto con el id indicado
+    stmt = db.select(Project).where(Project.id == project_id)
+    project_query = db.session.execute(stmt).first()
+    if not project_query:
+        raise InvalidProjectError('El proyecto indicado no existe')
+
+    # Convert fechas a tipo Date
+    y, m, d = start_date.split('-')
+    start_date_t = date(int(y), int(m), int(d))
+
+    y, m, d = deadline.split('-')
+    deadline_t = date(int(y), int(m), int(d))
+
+    # Chequea si los campos ingresados son válidos
+    validate_descrip_project(description)
+    validate_date(start_date_t, deadline_t)
+
+    # Actualiza los datos del proyecto
+    project_query[0].description = description.strip() 
+    project_query[0].start_date = start_date_t
+    project_query[0].end_date = deadline_t
+
+    db.session.commit()
