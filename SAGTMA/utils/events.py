f4d6db1edb1f77bd6cbee4ed9100a8cc639c82f5
admeneses <alejandrocardie12@gmail.com>
2023-03-01 22:31:04 -0400

    project events added

--- SAGTMA/utils/events.py
+++ SAGTMA/utils/events.py
@@ -3,7 +3,7 @@ from flask import session
 from SAGTMA.models import Event, db
 from SAGTMA.utils import auth
 
-
+# Modulo usuario
 def add_register(username: str):
     _add_event('Perfiles de Usuarios', f"Agregar usuario '{username}'")
 
@@ -13,10 +13,19 @@ def add_search_user(search: str, role: str):
         f"Buscar '{search}' en " + ('todos los roles' if not role else f"el rol '{role}'")
     )
 
+# Modulo proyecto
+def add_project(description: str):
+    _add_event('Portafolio de proyectos', f"Agregar proyecto '{description}'")
+
+def add_search_project(search: str):
+    _add_event('Portafolio de proyectos', f"Buscar '{search}'")
+
+# Modulo evento
 def add_search_log(search: str):
     _add_event('Logger de Eventos', f"Buscar '{search}'")
 
 def _add_event(module: str, description: str):
+    '''Crea y anade un nuevo a la base de datos'''
     current_user = auth.get_current_user(session['id'])
     new_event = Event(
         current_user,
@@ -25,3 +34,4 @@ def _add_event(module: str, description: str):
     )
     db.session.add(new_event)
     db.session.commit()
+
