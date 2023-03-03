f0a4acaf4525724830352a0e305d6812fa9f864f
chrischriscris <18-10892@usb.ve>
2023-03-03 16:58:07 -0400

    code formatted with black

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,9 +4,9 @@ from tests import BaseTestClass
 
 class TestWebApp(BaseTestClass):
     def test_app(self):
-        '''Testea que la aplicacion se crea correctamente y está en modo
-        testing'''
+        """Testea que la aplicacion se crea correctamente y está en modo
+        testing"""
         assert not create_app().testing
-        assert create_app({'TESTING': True}).testing
+        assert create_app({"TESTING": True}).testing
         assert self.app is not None
         assert self.app.testing
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -5,20 +5,23 @@ from tests import BaseTestClass
 
 class TestAuth(BaseTestClass):
     def test_register_invalid_password(self):
-        ''' Testea la creación de usuarios con la contraseñas inválidas.
+        """Testea la creación de usuarios con la contraseñas inválidas.
 
         Una contraseña es válida si:
           -Tiene al menos 6 caracteres y a lo sumo 20 caracteres
           -Tiene al menos un número
           -Tiene al menos una mayúscula y una minúscula
           -Tiene al menos un caracter especial
-        '''
+        """
+
         def test_register_user(password_to_test: str):
             with self.assertRaises(users.InvalidPasswordError):
                 users.register_user(
-                    "Alice", "Alice@example.com",
-                    password_to_test, password_to_test,
-                    Role.USER
+                    "Alice",
+                    "Alice@example.com",
+                    password_to_test,
+                    password_to_test,
+                    Role.USER,
                 )
 
         # Menos de 6 caracteres
@@ -40,12 +43,15 @@ class TestAuth(BaseTestClass):
         test_register_user("Alice123")
 
     def test_register_valid_password(self):
-        ''' Testea la creación de usuarios con la contraseñas válidas.'''
+        """Testea la creación de usuarios con la contraseñas válidas."""
+
         def register_test_user(id: int, password_to_test: str):
             users.register_user(
-                f"user_{id}", f"user_{id}@example.com",
-                password_to_test, password_to_test,
-                Role.USER
+                f"user_{id}",
+                f"user_{id}@example.com",
+                password_to_test,
+                password_to_test,
+                Role.USER,
             )
 
         # Creación de usuario con contraseña válida de 6 caracteres
@@ -55,136 +61,115 @@ class TestAuth(BaseTestClass):
         self.assertIsNone(register_test_user(2, "AliceTheBest123....."))
 
     def test_register_no_empty_allowed(self):
-        ''' Testea la creación de usuarios con campos vacíos.'''
+        """Testea la creación de usuarios con campos vacíos."""
         with self.assertRaises(users.MissingFieldError):
             users.register_user(
-                "", "Alice@example.com",
-                "Alice123.", "Alice123.",
-                Role.USER
+                "", "Alice@example.com", "Alice123.", "Alice123.", Role.USER
+            )
+
+        with self.assertRaises(users.MissingFieldError):
+            users.register_user("Alice", "", "Alice123.", "Alice123.", Role.USER)
+
+        with self.assertRaises(users.MissingFieldError):
+            users.register_user(
+                "Alice", "Alice@example.com", "", "Alice123.", Role.USER
             )
 
         with self.assertRaises(users.MissingFieldError):
             users.register_user(
-                "Alice", "",
-                "Alice123.", "Alice123.",
-                Role.USER
-            )
-
-        with self.assertRaises(users.MissingFieldError):
-            users.register_user(
-                "Alice", "Alice@example.com",
-                "", "Alice123.",
-                Role.USER
-            )
-
-        with self.assertRaises(users.MissingFieldError):
-            users.register_user(
-                "Alice", "Alice@example.com",
-                "Alice123.", "",
-                Role.USER
+                "Alice", "Alice@example.com", "Alice123.", "", Role.USER
             )
 
     def test_register_invalid_user(self):
-        '''Testea la creación de usuarios con nombre de usuario inválidos
+        """Testea la creación de usuarios con nombre de usuario inválidos
 
         Un nombre de usuario es válido si:
           -Tiene al menos 3 caracteres y a lo sumo 20 caracteres
           -No tiene caracteres especiales distintos de '_' (guión bajo)
           -No comienza con un caracter numérico
-        '''
+        """
+
         def test_register_user(username: str):
             with self.assertRaises(users.InvalidUsernameError):
                 users.register_user(
-                    username, f"{username}@example.com",
-                    'Hola123.', 'Hola123.',
-                    Role.USER
+                    username,
+                    f"{username}@example.com",
+                    "Hola123.",
+                    "Hola123.",
+                    Role.USER,
                 )
 
         # Con menos de 3 caracteres
-        test_register_user('ka')
+        test_register_user("ka")
 
         # Con más de 20 caracteres
-        test_register_user('John_Doe_The_Best_Username')
+        test_register_user("John_Doe_The_Best_Username")
 
         # Con caracteres especiales
-        test_register_user('val.')
+        test_register_user("val.")
 
         # Empieza con un número
-        test_register_user('1user')
+        test_register_user("1user")
 
     def test_register_mismatched_password(self):
-        '''Testea la creación de usuarios cuando la contraseña y su
+        """Testea la creación de usuarios cuando la contraseña y su
         verificacion no coinciden
-        '''
+        """
         with self.assertRaises(users.PasswordMismatchError):
             users.register_user(
-                "Alice", "Alice@example.com",
-                "Alice123.", "Alice12.",
-                Role.USER
+                "Alice", "Alice@example.com", "Alice123.", "Alice12.", Role.USER
             )
 
     def test_register_duplicated_username(self):
-        '''Testea la creación de usuarios con nombre de usuario o correo ya
+        """Testea la creación de usuarios con nombre de usuario o correo ya
         registrados
-        '''
+        """
         # Crea el usuario base
         users.register_user(
-            "Alice", "Alice@example.com",
-            "Alice123.", "Alice123.",
-            Role.USER
+            "Alice", "Alice@example.com", "Alice123.", "Alice123.", Role.USER
         )
 
         with self.assertRaises(users.AlreadyExistingUserError):
             # Usuario ya registrado
             users.register_user(
-                "Alice","Alice_dup@example.com",
-                "Alice123.", "Alice123.",
-                Role.USER
+                "Alice", "Alice_dup@example.com", "Alice123.", "Alice123.", Role.USER
             )
 
         with self.assertRaises(users.AlreadyExistingUserError):
             # Correo ya registrado
             users.register_user(
-                "Alice_dup","Alice@example.com",
-                "Alice123.", "Alice123.",
-                Role.USER
+                "Alice_dup", "Alice@example.com", "Alice123.", "Alice123.", Role.USER
             )
 
     def test_register_invalid_email(self):
-        '''Se testea la creación de usuarios con correos inválidos'''
+        """Se testea la creación de usuarios con correos inválidos"""
+
         def test_register_user(email: str):
             with self.assertRaises(users.InvalidEmailError):
                 users.register_user(
-                    'generic_user', email,
-                    'Hola123.', 'Hola123.',
-                    Role.USER
+                    "generic_user", email, "Hola123.", "Hola123.", Role.USER
                 )
 
-        test_register_user('Aliceexample.com')
-        test_register_user('Alice@examplecom')
-        test_register_user('Alice@example.a')
-        test_register_user('Alice@example.')
-        test_register_user('.@example.')
-        test_register_user('.@example.com')
+        test_register_user("Aliceexample.com")
+        test_register_user("Alice@examplecom")
+        test_register_user("Alice@example.a")
+        test_register_user("Alice@example.")
+        test_register_user(".@example.")
+        test_register_user(".@example.com")
 
     def test_login_incorrect_password(self):
-        '''Testea el inicio de sesión con contraseña incorrecta
-        '''
+        """Testea el inicio de sesión con contraseña incorrecta"""
         users.register_user(
-            "Alice", "Alice@example.com",
-            "Alice123.", "Alice123.",
-            Role.USER
+            "Alice", "Alice@example.com", "Alice123.", "Alice123.", Role.USER
         )
 
         with self.assertRaises(users.IncorrectPasswordError):
-            users.log_user("Alice","Alice12.")
+            users.log_user("Alice", "Alice12.")
 
     def test_login_correct_password(self):
-        '''Testea el inicio de sesión con contraseña correcta'''
+        """Testea el inicio de sesión con contraseña correcta"""
         users.register_user(
-            "Alice", "Alice@example.com",
-            "Alice123.", "Alice123.",
-            Role.USER
+            "Alice", "Alice@example.com", "Alice123.", "Alice123.", Role.USER
         )
 
-        self.assertIsNotNone(users.log_user("Alice","Alice123."))
+        self.assertIsNotNone(users.log_user("Alice", "Alice123."))
