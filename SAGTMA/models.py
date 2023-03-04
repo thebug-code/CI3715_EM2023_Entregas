377e6e93a9f7f4d7753100ecaf2901f330fb802c
chrischriscris <18-10892@usb.ve>
2023-03-04 14:46:04 -0400

    añadidos modelos base para iteracion 2

--- SAGTMA/models.py
+++ SAGTMA/models.py
@@ -8,7 +8,7 @@ class Role(db.Model):
     """Modelo de rol."""
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(80), unique=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
     users = db.relationship("User", backref="role")
 
     def __init__(self, name: str):
@@ -18,11 +18,27 @@ class Role(db.Model):
         return f"Role<{self.name}>"
 
 
+class Color(db.Model):
+    """Modelo de color de un vehículo."""
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    hex = db.Column(db.String(7), unique=True, nullable=False)
+    vehicles = db.relationship("Vehicle", backref="color")
+
+    def __init__(self, name: str, hex: str):
+        self.name = name
+        self.hex = hex
+
+    def __repr__(self) -> str:
+        return f"Color<{self.name}: {self.hex}>"
+
+
 user_project = db.Table(
     "user_project",
     db.Column("id", db.Integer, primary_key=True, autoincrement=True),
-    db.Column("user_id", db.Integer, db.ForeignKey("user.id")),
-    db.Column("project_id", db.Integer, db.ForeignKey("project.id")),
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id"), nullable=False),
+    db.Column("project_id", db.Integer, db.ForeignKey("project.id"), nullable=False),
     db.UniqueConstraint("user_id", "project_id"),
 )
 
@@ -31,10 +47,10 @@ class User(db.Model):
     """Modelo de usuario."""
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(20), unique=True)
-    names = db.Column(db.String(50))
-    surnames = db.Column(db.String(50))
-    password = db.Column(db.String(80))
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    names = db.Column(db.String(50), nullable=False)
+    surnames = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
     role_id = db.Column(db.Integer, db.ForeignKey("role.id"), nullable=False)
 
     # Relación n:n entre usuarios y proyectos
@@ -62,10 +78,10 @@ class Project(db.Model):
     """Modelo de proyecto."""
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    description = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(100), unique=True, nullable=False)
     active = db.Column(db.Boolean, default=False)
-    start_date = db.Column(db.Date)
-    end_date = db.Column(db.Date)
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
 
     def __init__(self, description: str, start_date, end_date):
         self.description = description
@@ -81,8 +97,8 @@ class Event(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    module = db.Column(db.String(80))
-    description = db.Column(db.String(80))
+    module = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(80), nullable=False)
     time = db.Column(db.DateTime(timezone=True), default=datetime.datetime.now)
 
     def __init__(self, user: User, module: str, description: str):
@@ -94,3 +110,78 @@ class Event(db.Model):
 
     def __repr__(self) -> str:
         return f"Event<{self.user.username}: {self.module} - {self.type}>"
+
+
+class Client(db.Model):
+    """Modelo de cliente."""
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id_number = db.Column(db.String(10), unique=True, nullable=False)
+    names = db.Column(db.String(50), nullable=False)
+    surnames = db.Column(db.String(50), nullable=False)
+    birthdate = db.Column(db.Date, nullable=False)
+    phone_number = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    vehicles = db.relationship("Vehicle", backref="owner", cascade="all, delete-orphan")
+
+    def __init__(
+        self,
+        id_number: str,
+        names: str,
+        surnames: str,
+        birthdate,
+        phone_number: str,
+        email: str,
+        address: str,
+    ):
+        self.id_number = id_number
+        self.names = names
+        self.surnames = surnames
+        self.birthdate = birthdate
+        self.phone_number = phone_number
+        self.email = email
+        self.address = address
+
+    def __repr__(self) -> str:
+        return f"Client<{self.id_number}: {self.names} {self.surnames}>"
+
+
+class Vehicle(db.Model):
+    """Modelo de vehículo."""
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    license_plate = db.Column(db.String(10), unique=True, nullable=False)
+    brand = db.Column(db.String(50), nullable=False)
+    model = db.Column(db.String(50), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    body_number = db.Column(db.String(20), nullable=False)
+    engine_number = db.Column(db.String(20), nullable=False)
+    color_id = db.Column(db.Integer, db.ForeignKey("color.id"), nullable=False)
+    problem = db.Column(db.String(120), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey("client.id"), nullable=False)
+
+    def __init__(
+        self,
+        license_plate: str,
+        brand: str,
+        model: str,
+        year: int,
+        body_number: str,
+        engine_number: str,
+        color: Color,
+        problem: str,
+        owner: Client,
+    ):
+        self.license_plate = license_plate
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.body_number = body_number
+        self.engine_number = engine_number
+        self.color = color
+        self.problem = problem
+        self.owner = owner
+
+    def __repr__(self) -> str:
+        return f"Vehicle<{self.license_plate}: {self.brand} {self.model}>"
