7f698ff542390ad7e731b377e7563e2d1c0e17e4
chrischriscris <18-10892@usb.ve>
2023-03-08 14:03:52 -0400

    copiadas plantillas detalles de vehículos

--- SAGTMA/routes/analyst.py
+++ SAGTMA/routes/analyst.py
@@ -14,6 +14,9 @@ from SAGTMA.utils.decorators import requires_roles
 from SAGTMA.models import Client, db
 
 
+# ========== CLIENTES ==========
+
+
 @current_app.route("/clients-details/", methods=["GET", "POST"])
 @requires_roles("Analista de Operaciones")
 def clients_details() -> Response:
@@ -55,6 +58,71 @@ def clients_details() -> Response:
 @current_app.route("/clients-details/register/", methods=["POST"])
 @requires_roles("Analista de Operaciones")
 def register_client() -> Response:
+    """Registra un cliente en la base de datos."""
+    id_number = request.form.get("id-number")
+    names = request.form.get("names")
+    surnames = request.form.get("surnames")
+    birthdate = request.form.get("birthdate")
+    phone_number = request.form.get("phone-number")
+    email = request.form.get("email")
+    address = request.form.get("address")
+
+    try:
+        clients.register_client(
+            id_number, names, surnames, phone_number, email, address
+        )
+    except clients.ClientError as e:
+        flash(f"{e}")
+
+    # Se permanece en la página
+    flash("Cliente añadido exitosamente")
+    return redirect(url_for("clients_details"))
+
+
+# ========== VEHÍCULOS ==========
+
+
+@current_app.route("/clients-details/<int:client_id>/", methods=["GET", "POST"])
+@requires_roles("Analista de Operaciones")
+def vehicle_details(client_id: int) -> Response:
+    """Muestra la lista de clientes añadidos en el sistema"""
+    # SELECT * FROM client
+    stmt = db.select(Client)
+
+    if request.method == "POST":
+        # Obtiene los datos del formulario
+        client = request.form.get("client-filter")
+
+        if client:
+            # WHERE (names || surnames) LIKE '%client%' OR
+            #     id_number LIKE '%client%' OR
+            #     phone_number LIKE '%client%' OR
+            #     email LIKE '%client%' OR
+            #     address LIKE '%client%'
+            stmt = stmt.where(
+                db.or_(
+                    db.func.lower(Client.names + " " + Client.surnames).like(
+                        f"%{client}%"
+                    ),
+                    Client.id_number.like(f"%{client}%"),
+                    Client.phone_number.like(f"%{client}%"),
+                    Client.email.like(f"%{client}%"),
+                    Client.address.like(f"%{client}%"),
+                )
+            )
+
+        # Añade el evento de búsqueda
+        events.add_search_client(client)
+
+    result = db.session.execute(stmt).fetchall()
+    _clients = [r for r, in result]
+
+    return render_template("analyst/clients.html", clients=_clients)
+
+
+@current_app.route("/clients-details/<int:client_id>/register/", methods=["POST"])
+@requires_roles("Analista de Operaciones")
+def register_vehicle(client_id: int) -> Response:
     """Registra un cliente en la base de datos."""
     id_number = request.form.get("id-number")
     names = request.form.get("names")
