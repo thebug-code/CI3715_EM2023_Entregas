31584c51a0026b33ef8f1a0d7257aaea2cd5f90d
chrischriscris <18-10892@usb.ve>
2023-03-12 02:03:26 -0400

    template for selenium testing

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,6 @@
 import unittest
+from selenium import webdriver
+from os import environ
 
 from SAGTMA import create_app
 
@@ -22,6 +24,30 @@ class BaseTestClass(unittest.TestCase):
         # Puebla la base de datos para el conjunto de tests, si es necesario
         if hasattr(self, "populate_db"):
             self.populate_db()
+        
+        # Inicializa el driver de Selenium, usa el webdriver especificado por
+        # la variable de entorno WEBDRIVER
+        webdriver_name = environ.get("WEBDRIVER")
+
+        # Añade la opción de headless dependiendo del driver
+        if webdriver_name == "Firefox":
+            from selenium.webdriver.firefox.options import Options
+
+            options = Options()
+            options.add_argument("headless")
+        elif webdriver_name == "Chrome":
+            from selenium.webdriver.chrome.options import Options
+
+            options = Options()
+            options.add_argument("headless")
+        elif webdriver_name == "Edge":
+            from selenium.webdriver.edge.options import Options
+            options = Options()
+            options.add_argument("headless")
+        else:
+            raise ValueError("Unsupported webdriver, must be one of: Firefox, Chrome, Edge")
+
+        self.driver = getattr(webdriver, webdriver_name)(options=options)
 
     def tearDown(self):
         # Elimina todas las tablas de la base de datos
@@ -31,3 +57,7 @@ class BaseTestClass(unittest.TestCase):
 
         # Elimina el contexto de la aplicación
         self.ctx.pop()
+
+        # Cierra el driver de Selenium
+        self.driver.close()
+        self.driver.quit()
--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,10 @@ class TestWebApp(BaseTestClass):
         assert create_app({"TESTING": True}).testing
         assert self.app is not None
         assert self.app.testing
+
+    def test_selenium(self):
+        """Testea que el driver de Selenium se crea correctamente"""
+        assert self.driver is not None
+
+        self.driver.get("http://localhost:5000/")
+        assert "SAGTMA" in self.driver.title
