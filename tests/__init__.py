0d92a29921e9a0558119f1e841bf9a6d2af83db5
chrischriscris <18-10892@usb.ve>
2023-03-12 17:41:57 -0400

    login tests added

--- tests/__init__.py
+++ tests/__init__.py
@@ -38,22 +38,6 @@ class BaseTestClass(unittest.TestCase):
         self.driver = getattr(webdriver, webdriver_name)(options=options)
         self.base_url = "http://localhost:5001"
 
-    def _login(self, username: str, password: str):
-        '''Mét'''
-        self.driver.get(f"{self.base_url}/")
-        username_form = self.driver.find_element(By.ID, "username")
-        password_form = self.driver.find_element(By.ID, "password")
-        submit_button = self.driver.find_element(By.XPATH, "//button[@type='submit']")
-
-        username_form.send_keys(username)
-        password_form.send_keys(password)
-        submit_button.click()
-
-    def _logout(self):
-        self.driver.get(f"{self.base_url}/")
-        logout_button = self.driver.find_element(By.XPATH, "//a[@class='nav-link link']")
-        logout_button.click()
-
     def tearDown(self):
         # Cierra el driver de Selenium
         self.driver.close()
--- tests/test_login.py
+++ tests/test_login.py
@@ -6,200 +6,37 @@ from selenium.webdriver.common.by import By
 
 
 class TestLogin(BaseTestClass):
-    def test_register_invalid_password(self):
-        """Testea la creación de usuarios con la contraseñas inválidas.
-
-        Una contraseña es válida si:
-          -Tiene al menos 6 caracteres y a lo sumo 20 caracteres
-          -Tiene al menos un número
-          -Tiene al menos una mayúscula y una minúscula
-          -Tiene al menos un caracter especial
-        """
-
-        # def test_register_user(password_to_test: str):
-        #     with self.assertRaises(users.InvalidPasswordError):
-        #         users.register_user(
-        #             "Alice",
-        #             "Alice@example.com",
-        #             password_to_test,
-        #             password_to_test,
-        #             Role.USER,
-        #         )
-
-        # # Menos de 6 caracteres
-        # test_register_user("Al1.")
-
-        # # Más de 20 caracteres
-        # test_register_user("AliceTheBest123_4567.")
-
-        # # Sin número
-        # test_register_user("Alice.")
-
-        # # Sin mayúsculas
-        # test_register_user("alice123.")
-
-        # # Sin minúsculas
-        # test_register_user("ALICE123.")
-
-        # # Sin caracteres especiales
-        # test_register_user("Alice123")
-
-    def test_register_valid_password(self):
-        """Testea la creación de usuarios con la contraseñas válidas."""
-
-        # def register_test_user(id: int, password_to_test: str):
-        #     users.register_user(
-        #         f"user_{id}",
-        #         f"user_{id}@example.com",
-        #         password_to_test,
-        #         password_to_test,
-        #         Role.USER,
-        #     )
-
-        # # Creación de usuario con contraseña válida de 6 caracteres
-        # self.assertIsNone(register_test_user(1, "Alice1."))
-
-        # # Creación de usuario con contraseña válida de 20 caracteres
-        # self.assertIsNone(register_test_user(2, "AliceTheBest123....."))
-
-    def test_register_no_empty_allowed(self):
-        """Testea la creación de usuarios con campos vacíos."""
-        # with self.assertRaises(users.MissingFieldError):
-        #     users.register_user(
-        #         "", "Alice@example.com", "Alice123.", "Alice123.", Role.USER
-        #     )
-
-        # with self.assertRaises(users.MissingFieldError):
-        #     users.register_user("Alice", "", "Alice123.", "Alice123.", Role.USER)
-
-        # with self.assertRaises(users.MissingFieldError):
-        #     users.register_user(
-        #         "Alice", "Alice@example.com", "", "Alice123.", Role.USER
-        #     )
-
-        # with self.assertRaises(users.MissingFieldError):
-        #     users.register_user(
-        #         "Alice", "Alice@example.com", "Alice123.", "", Role.USER
-        #     )
-
-    def test_register_invalid_user(self):
-        """Testea la creación de usuarios con nombre de usuario inválidos
-
-        Un nombre de usuario es válido si:
-          -Tiene al menos 3 caracteres y a lo sumo 20 caracteres
-          -No tiene caracteres especiales distintos de '_' (guión bajo)
-          -No comienza con un caracter numérico
-        """
-
-        # def test_register_user(username: str):
-        #     with self.assertRaises(users.InvalidUsernameError):
-        #         users.register_user(
-        #             username,
-        #             f"{username}@example.com",
-        #             "Hola123.",
-        #             "Hola123.",
-        #             Role.USER,
-        #         )
-
-        # # Con menos de 3 caracteres
-        # test_register_user("ka")
-
-        # # Con más de 20 caracteres
-        # test_register_user("John_Doe_The_Best_Username")
-
-        # # Con caracteres especiales
-        # test_register_user("val.")
-
-        # # Empieza con un número
-        # test_register_user("1user")
-
-    def test_register_mismatched_password(self):
-        """Testea la creación de usuarios cuando la contraseña y su
-        verificacion no coinciden
-        """
-        # with self.assertRaises(users.PasswordMismatchError):
-        #     users.register_user(
-        #         "Alice", "Alice@example.com", "Alice123.", "Alice12.", Role.USER
-        #     )
-
-    def test_register_duplicated_username(self):
-        """Testea la creación de usuarios con nombre de usuario o correo ya
-        registrados
-        """
-        # # Crea el usuario base
-        # users.register_user(
-        #     "Alice", "Alice@example.com", "Alice123.", "Alice123.", Role.USER
-        # )
-
-        # with self.assertRaises(users.AlreadyExistingUserError):
-        #     # Usuario ya registrado
-        #     users.register_user(
-        #         "Alice", "Alice_dup@example.com", "Alice123.", "Alice123.", Role.USER
-        #     )
-
-        # with self.assertRaises(users.AlreadyExistingUserError):
-        #     # Correo ya registrado
-        #     users.register_user(
-        #         "Alice_dup", "Alice@example.com", "Alice123.", "Alice123.", Role.USER
-        #     )
-
-    def test_register_invalid_email(self):
-        """Se testea la creación de usuarios con correos inválidos"""
-
-        # def test_register_user(email: str):
-        #     with self.assertRaises(users.InvalidEmailError):
-        #         users.register_user(
-        #             "generic_user", email, "Hola123.", "Hola123.", Role.USER
-        #         )
-
-        # test_register_user("Aliceexample.com")
-        # test_register_user("Alice@examplecom")
-        # test_register_user("Alice@example.a")
-        # test_register_user("Alice@example.")
-        # test_register_user(".@example.")
-        # test_register_user(".@example.com")
-
-    def test_login_incorrect_user(self):
-        self._login("chus", "contraseña falsa")
-
-        self.assertIn("Iniciar Sesión", self.driver.title)
-        self.assertIn("El usuario ingresado no existe", self.driver.page_source)
-        self.assertNotIn("Cerrar sesión", self.driver.page_source)
+    def test_login(self):
+        '''Testea el login con credenciales correctas.'''
+        self.driver.get("http://localhost:5001/login/")
+        self.driver.find_element(By.ID, "username").send_keys("admin")
+        self.driver.find_element(By.ID, "password").click()
+        self.driver.find_element(By.ID, "password").send_keys("Admin123.")
+        self.driver.find_element(By.CSS_SELECTOR, ".btn-primary").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".title-container").click()
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".title-container")
+        self.assertTrue(len(elements) > 0)
 
     def test_login_incorrect_password(self):
-        """Testea el inicio de sesión con contraseña incorrecta"""
-        self._login("admin", "Admin123")
-
-        self.assertIn("Iniciar Sesión", self.driver.title)
-        self.assertIn(
-            "Las credenciales ingresadas son incorrectas", self.driver.page_source
-        )
-        self.assertNotIn("Cerrar sesión", self.driver.page_source)
-
-    def test_login_correct_password(self):
-        """Testea el inicio de sesión con contraseña correcta"""
-        self._login("admin", "Admin123.")
-
-        self.assertIn("Perfiles de Usuarios", self.driver.title)
-        self.assertIn("Cerrar sesión", self.driver.page_source)
-    
-    def test_login_empty_fields(self):
-        """Testea el inicio de sesión con campos vacíos"""
-        self._login("", "")
-
-        self.assertIn("Iniciar Sesión", self.driver.title)
-        self.assertIn("Necesitas iniciar sesión primero", self.driver.page_source)
-        self.assertNotIn("Cerrar sesión", self.driver.page_source)
+        '''Testea el login con credenciales incorrectas.'''
+        self.driver.get("http://localhost:5001/login/")
+        self.driver.find_element(By.ID, "username").send_keys("admin")
+        self.driver.find_element(By.ID, "username").send_keys(Keys.ENTER)
+        self.driver.find_element(By.ID, "password").click()
+        self.driver.find_element(By.ID, "password").click()
+        self.driver.find_element(By.ID, "password").send_keys("admin123..")
+        self.driver.find_element(By.CSS_SELECTOR, ".btn-primary").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".toast-body").click()
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".toast-body")
+        self.assertTrue(len(elements) > 0)
 
     def test_logout(self):
         """Testea el cierre de sesión"""
-        self._login("admin", "Admin123.")
-
-        self.assertIn("Perfiles de Usuarios", self.driver.title)
-        self.assertIn("Cerrar sesión", self.driver.page_source)
-
-        self._logout()
-
-        self.assertIn("Iniciar Sesión", self.driver.title)
-        self.assertIn("Iniciar sesión", self.driver.page_source)
-        self.assertNotIn("Cerrar sesión", self.driver.page_source)
+        self.driver.get("http://localhost:5001/login/")
+        self.driver.find_element(By.ID, "username").send_keys("admin")
+        self.driver.find_element(By.ID, "password").click()
+        self.driver.find_element(By.ID, "password").send_keys("Admin123.")
+        self.driver.find_element(By.CSS_SELECTOR, ".btn-primary").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".title-container").click()
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".title-container")
+        self.assertTrue(len(elements) > 0)
