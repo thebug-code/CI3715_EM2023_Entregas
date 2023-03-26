3a16038583e9b53c7e69935fe13696c2b52972dc
chrischriscris <18-10892@usb.ve>
2023-03-26 17:03:12 -0400

    added 3 tests for login

--- tests/unittests/test_login.py
+++ tests/unittests/test_login.py
@@ -4,12 +4,35 @@ from tests.unittests import BaseTestClass
 class TestLogin(BaseTestClass):
     def test_login(self):
         """Testea el login con credenciales correctas."""
-        pass
+        res = self.client.post(
+            "/login/",
+            data={"username": "admin", "password": "Admin123."},
+            follow_redirects=True,
+        )
+
+        self.assertNotIn(b"Iniciar Sesi", res.data)
 
     def test_login_incorrect_password(self):
         """Testea el login con credenciales incorrectas."""
-        pass
+        res = self.client.post(
+            "/login/",
+            data={"username": "admin", "password": "Admin123.."},
+            follow_redirects=True,
+        )
+
+        self.assertIn(b"Iniciar Sesi", res.data)
 
     def test_logout(self):
         """Testea el cierre de sesión"""
-        pass
+        self.client.post(
+            "/login/",
+            data={"username": "admin", "password": "Admin123."},
+            follow_redirects=True,
+        )
+        
+        res = self.client.post(
+            "/logout/",
+            follow_redirects=True,
+        )
+
+        self.assertIn(b"Iniciar Sesi", res.data)
