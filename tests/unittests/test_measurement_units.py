0b80b6d6ba4b6b9b0a291d271a95b92ee14efb2d
chrischriscris <18-10892@usb.ve>
2023-04-17 17:48:43 -0400

    terminados tests unitarios de unidades de medida

--- tests/unittests/test_measurement_units.py
+++ tests/unittests/test_measurement_units.py
@@ -4,11 +4,7 @@ from SAGTMA.models import (
     User,
     Role,
     db,
-    Project,
-    Client,
-    Vehicle,
-    Department,
-    ProjectDetail,  # Dejar los que hacen falta
+    MeasureUnit,
 )
 from SAGTMA.utils.auth import hash_password
 import SAGTMA.utils.measurement_units as mu
@@ -19,15 +15,27 @@ class MeasurementUnitsTests(BaseTestClass):
         super().populate_db()
 
         # Añade una unidad de medida
-        dept = Department("Eléctrica")
-        dept.id = 0
+        unit = MeasureUnit(6, "Centímetros")
+        unit.id = 0
 
-        db.session.add(dept)
+        db.session.add(unit)
         db.session.commit()
 
     def _login_admin(self):
         """Inicia sesión con un usuario Administrador."""
         return self.login_user("admin", "Admin123.")
+
+    def _post_register_unit(self, data: dict):
+        """Envía una petición POST para registrar una unidad de medida."""
+        return self.client.post(
+            "/measurement-units/register/", data=data, follow_redirects=True
+        )
+
+    def _post_edit_unit(self, data: dict):
+        """Envía una petición POST para editar una unidad de medida."""
+        return self.client.post(
+            f"/measurement-units/{data['id']}/edit/", data=data, follow_redirects=True
+        )
 
     def test_validate_dimension_valid(self):
         """Testea la validación de dimensiones válidas."""
@@ -74,3 +82,79 @@ class MeasurementUnitsTests(BaseTestClass):
         _test("")
         _test("cm2")
         _test("Pulgadas-cuadradas")
+
+    def test_register_unit_valid(self):
+        """Testea el registro de una unidad de medida válida"""
+        self._login_admin()
+
+        # Datos de unidad válidos
+        self._post_register_unit({"dimension": "1", "unit": "Pulgada"})
+
+        stmt = db.select(MeasureUnit).where(MeasureUnit.unit == "Pulgada")
+        self.assertIsNotNone(db.session.execute(stmt).first())
+
+    def test_register_unit_invalid(self):
+        """Testea el registro de una unidad de medida inválida"""
+        self._login_admin()
+
+        # Datos de unidad inválidos
+        self._post_register_unit({"dimension": "0", "unit": "Pulgada"})
+
+        stmt = db.select(MeasureUnit).where(MeasureUnit.unit == "Pulgada")
+        self.assertIsNone(db.session.execute(stmt).first())
+
+        self._post_register_unit({"dimension": "1", "unit": ""})
+
+        stmt = db.select(MeasureUnit).where(MeasureUnit.unit == "")
+        self.assertIsNone(db.session.execute(stmt).first())
+
+    def test_edit_unit_valid(self):
+        """Testea la edición de una unidad de medida válida"""
+        self._login_admin()
+
+        # Datos de unidad válidos
+        self._post_edit_unit(data={"dimension": "10", "unit": "Metros", "id": 0})
+
+        stmt = db.select(MeasureUnit).where(MeasureUnit.unit == "Metros")
+        self.assertIsNotNone(db.session.execute(stmt).first())
+
+        stmt = db.select(MeasureUnit).where(MeasureUnit.unit == "Centímetros")
+        self.assertIsNone(db.session.execute(stmt).first())
+
+    def test_edit_unit_invalid(self):
+        """Testea la edición de una unidad de medida inválida"""
+        self._login_admin()
+
+        # Datos de unidad inválidos
+        self._post_edit_unit(data={"dimension": "0", "unit": "m2", "id": 0})
+
+        stmt = db.select(MeasureUnit).where(MeasureUnit.unit == "m2")
+        self.assertIsNone(db.session.execute(stmt).first())
+
+        self._post_edit_unit(data={"dimension": "1", "unit": "", "id": 0})
+
+        stmt = db.select(MeasureUnit).where(MeasureUnit.unit == "")
+        self.assertIsNone(db.session.execute(stmt).first())
+
+        stmt = db.select(MeasureUnit).where(MeasureUnit.unit == "Centímetros")
+        self.assertIsNotNone(db.session.execute(stmt).first())
+
+    def test_delete_unit_valid(self):
+        """Testea la eliminación de una unidad de medida de forma válida."""
+        self._login_admin()
+
+        # Elimina la unidad de medida
+        self.client.post("/measurement-units/0/delete/", follow_redirects=True)
+
+        stmt = db.select(MeasureUnit).where(MeasureUnit.id == 0)
+        self.assertIsNone(db.session.execute(stmt).first())
+
+    def test_delete_unit_invalid(self):
+        """Testea la eliminación de una unidad de medida de forma inválida."""
+        self._login_admin()
+
+        # Elimina la unidad de medida
+        self.client.post("/measurement-units/1/delete/", follow_redirects=True)
+
+        stmt = db.select(MeasureUnit).where(MeasureUnit.id == 0)
+        self.assertIsNotNone(db.session.execute(stmt).first())
